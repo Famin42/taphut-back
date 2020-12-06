e158a525c33902b0776d7be64a142017a0bfe3ae
Alex <alexfomin2402@gmail.com>
2020-12-07 00:42:13 +0300

    update: update test-cli script

--- src/script/test-cli.ts
+++ src/script/test-cli.ts
@@ -4,23 +4,16 @@ import logger, { setDebugLevel } from 'utils/logger';
 
 setDebugLevel(process.env.DEBUG_LEVEL || 'info');
 
-// const stringCommand = '--help';
-const stringCommand = 'filter-list';
-// const stringCommand = 'filter-get';
-// const stringCommand = 'filter-create';
-// const stringCommand = 'filter-delete';
-// const stringCommand = 'filter-update';
 const chatId = mustEnv('CHAT_ID');
 const token = mustEnv('TG_BOT_TOKEN');
 
 function main(): void {
   try {
-    logger.debug(`stringCommand: ${JSON.stringify(stringCommand, null, 2)}`);
+    const command = process.argv.slice(2).reduce((command, str) => command.concat(` ${str}`), '');
 
     const parser = buildParser({ chatId, token });
-    logger.info(`buildParser: Success`);
 
-    parser(stringCommand);
+    parser(command);
     logger.debug(`execute parser: Success`);
   } catch (error) {
     logger.error(error.message);
