3320b8c68ba63b6cad819348d9027d9f399b46b9
Alex <alexfomin2402@gmail.com>
2020-12-06 16:03:00 +0300

    feat: add simple script to invoke CLI locally

---
+++ src/script/test-cli.ts
@@ -0,0 +1,29 @@
+import { buildParser } from 'telegram-bot/cli';
+import logger, { setDebugLevel } from 'utils/logger';
+
+setDebugLevel(process.env.DEBUG_LEVEL || 'info');
+
+// const stringCommand = '--help';
+// const stringCommand = 'filter-list';
+// const stringCommand = 'filter-get';
+// const stringCommand = 'filter-create';
+// const stringCommand = 'filter-delete';
+const stringCommand = 'filter-update';
+const chatId = 'chatId';
+const token = 'token';
+
+function main(): void {
+  try {
+    logger.debug(`stringCommand: ${JSON.stringify(stringCommand, null, 2)}`);
+
+    const parser = buildParser({ chatId, token });
+    logger.info(`buildParser: Success`);
+
+    parser(stringCommand);
+    logger.debug(`execute parser: Success`);
+  } catch (error) {
+    logger.error(error.message);
+  }
+}
+
+main();
