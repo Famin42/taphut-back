9ada5286032159df6a6c7cd72a1e4b004b952b00
Alex <alexfomin2402@gmail.com>
2020-12-18 11:38:27 +0300

    chore: remove to lowerCase

--- src/telegram-bot/webhook.ts
+++ src/telegram-bot/webhook.ts
@@ -48,7 +48,7 @@ async function processMessage({ body }: APIGatewayEvent): Promise<IHttpResponse>
   logger.info(`Build Parser`);
 
   const parser = buildParser({ chatId: chat_id, token });
-  parser((stringCommand as string).toLowerCase());
+  parser(stringCommand);
 
   return httpSuccess();
 }
